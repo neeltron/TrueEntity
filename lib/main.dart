16a781d1708eeadf53d174060724f54c503106bf
Neel Adwani <nprosofficial@gmail.com>
2023-09-23 04:42:04 -0400

    Added Inputs for getting Personal Information

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Empty Classy Page'),
+        title: const Text('Empty Classy Page'),
       ),
       body: Center(
         child: ElevatedButton(
@@ -36,7 +36,7 @@ class MyHomePage extends StatelessWidget {
               builder: (context) => const NextPage(),
             ));
           },
-          child: Text(
+          child: const Text(
             'Go to Next Page',
             style: TextStyle(fontSize: 18),
           ),
@@ -46,19 +46,61 @@ class MyHomePage extends StatelessWidget {
   }
 }
 
-class NextPage extends StatelessWidget {
+class NextPage extends StatefulWidget {
   const NextPage();
+
+  @override
+  _NextPageState createState() => _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ssnController = TextEditingController();
+  TextEditingController passportController = TextEditingController();
+  TextEditingController dobController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Next Page'),
+        title: const Text('Next Page'),
       ),
-      body: Center(
-        child: Text(
-          'This is the next page!',
-          style: TextStyle(fontSize: 24),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(labelText: 'Name'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: ssnController,
+              decoration: const InputDecoration(labelText: 'Social Security Number'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: passportController,
+              decoration: const InputDecoration(labelText: 'Passport Number'),
+            ),
+            const SizedBox(height: 16),
+            TextField(
+              controller: dobController,
+              decoration: const InputDecoration(labelText: 'Date of Birth'),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                final name = nameController.text;
+                final ssn = ssnController.text;
+                final passport = passportController.text;
+                final dob = dobController.text;
+
+              },
+              child: const Text('Submit'),
+            ),
+          ],
         ),
       ),
     );
